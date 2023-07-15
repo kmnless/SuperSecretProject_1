c52629f735367b3c6e6862f11ddb503bd794479b
Alexander Babko <106757034+kmnless@users.noreply.github.com>
2023-07-15 19:37:32 +0300

    perline noise and buildings placement algos

---
+++ Generation/BuildingsGenerator.cs
@@ -0,0 +1,147 @@
+﻿namespace Generation
+{
+    //try
+    //{
+    //    BuildingsGenerator bg = new(40, 40, 5, (int)DateTime.UtcNow.Ticks);
+    //bg.test();
+    //}
+    //catch (ArgumentException ex)
+    //{
+    //Console.WriteLine(ex.Message);
+    //}
+public class BuildingsGenerator
+    {
+        private readonly int minRadius;
+        private readonly int banRadius;
+        private Random random;
+        private int matrixSizeX;
+        private int matrixSizeY;
+        private int amountOfBases;
+        private int[,] matrix;
+        private HashSet<Tuple<int, int>> placedBases { get; }
+        private HashSet<Tuple<int, int>> placedFlags { get; }
+        private Tuple<int, int> middle;
+        public BuildingsGenerator(int sizeX, int sizeY, int amountOfBases, int seed)
+        {
+            matrixSizeX = sizeX;
+            matrixSizeY = sizeY;
+            this.amountOfBases = amountOfBases;
+            middle = new(matrixSizeX / 2, matrixSizeY / 2);
+            matrix = new int[matrixSizeX, matrixSizeY];
+            minRadius = 3;
+            banRadius = 10;
+            random = new Random(seed);
+            placedBases = new();
+            placedFlags = new();
+        }
+
+        //public void test()
+        //{
+        //    placeBases();
+        //    placeNecessaryFlags();
+
+        //    foreach (Tuple<int, int> a in placedBases)
+        //    {
+        //        matrix[a.Item1, a.Item2] = 1;
+        //    }
+        //    foreach (Tuple<int, int> a in placedFlags)
+        //    {
+        //        matrix[a.Item1, a.Item2] = 7;
+        //    }
+
+        //    for (int x = 0; x < matrixSizeX; x++)
+        //    {
+        //        for (int y = 0; y < matrixSizeY; y++)
+        //        {
+        //            Console.Write(matrix[x, y]);
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //}
+
+        private bool placeBase(int depth = 0)
+        {
+            if (depth >= matrixSizeX * matrixSizeY)
+                return false;
+            int posX, posY;
+            posX = random.Next(0, matrixSizeX);
+            posY = random.Next(0, matrixSizeY);
+            Tuple<int, int> pos = new Tuple<int, int>(posX, posY);
+            if (baseCanBePlaced(pos))
+            {
+                placedBases.Add(pos);
+                return true;
+            }
+            else
+            {
+                depth++;
+                if (placeBase(depth))
+                    return true;
+                return false;
+            }
+        }
+        public void placeBases(int depth = 0)
+        {
+            if (depth > 5)
+                throw new ArgumentException("Wrong base count");
+            for (int i = 0; i < amountOfBases; ++i)
+                if (!placeBase())
+                {
+                    placedBases.Clear();
+                    placedFlags.Clear();
+                    depth++;
+                    placeBases(depth);
+                }
+        }
+        public void placeNecessaryFlags()
+        {
+            foreach (Tuple<int, int> bannedPos in placedBases)
+            {
+                placeFlagInBanRadius(bannedPos);
+            }
+        }
+        private void placeFlagInBanRadius(Tuple<int, int> basePos)
+        {
+
+            int posX = random.Next(basePos.Item1 - banRadius, basePos.Item1 + banRadius);
+            int posY = random.Next(basePos.Item2 - banRadius, basePos.Item2 + banRadius);
+            Tuple<int, int> expectedFlagPos = getNormalizedPos(posX, posY);
+            if (calculateDistance(basePos, expectedFlagPos) < banRadius && calculateDistance(basePos, expectedFlagPos) > minRadius)
+            {
+                placedFlags.Add(expectedFlagPos);
+            }
+            else
+            {
+                placeFlagInBanRadius(basePos);
+            }
+        }
+        private Tuple<int, int> getNormalizedPos(int posX, int posY)
+        {
+            if (posX < 0)
+                posX = 0;
+            if (posY < 0)
+                posY = 0;
+            if (posX >= matrixSizeX)
+                posX = matrixSizeX - 1;
+            if (posY >= matrixSizeY)
+                posY = matrixSizeY - 1;
+            return new Tuple<int, int>(posX, posY);
+        }
+        private bool baseCanBePlaced(Tuple<int, int> placePos)
+        {
+            if (calculateDistance(placePos, middle) < banRadius * 2)
+                return false;
+
+            foreach (Tuple<int, int> bannedPos in placedBases)
+            {
+                if (calculateDistance(placePos, bannedPos) < banRadius * 2)
+                    return false;
+            }
+            return true;
+        }
+        private int calculateDistance(Tuple<int, int> pos1, Tuple<int, int> pos2)
+        {
+            return Convert.ToInt32(Math.Sqrt((pos2.Item1 - pos1.Item1) * (pos2.Item1 - pos1.Item1) + (pos2.Item2 - pos1.Item2) * (pos2.Item2 - pos1.Item2)));
+        }
+    }
+}
---
+++ Generation/PerlineNoise.cs
@@ -0,0 +1,111 @@
+﻿namespace PerlineNoise
+{
+
+    public class PerlinNoise
+    {
+        private readonly int[] permutation;
+        private readonly int[] p;
+        private readonly Random random;
+        private int[] GetPermutation()
+        {
+            int[] output = new int[256];
+            byte current;
+            for (int i = 0; i < 256; ++i)
+            {
+                output[i] = -1;
+            }
+            for (int i = 0; i < 256; ++i)
+            {
+                current = (byte)random.Next();
+                if (!isIn(current, output))
+                {
+                    output[i] = current;
+                }
+                else
+                {
+                    i--;
+                }
+            }
+            return output;
+        }
+        private bool isIn(byte val, int[] array)
+        {
+            foreach (int a in array)
+            {
+                if (a == val)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public PerlinNoise(int seed)
+        {
+            random = new Random(seed);
+            permutation = GetPermutation();
+            p = new int[512];
+            for (int i = 0; i < 256; i++)
+            {
+                p[i] = permutation[i];
+                p[256 + i] = permutation[i];
+            }
+        }
+        private double Fade(double t)
+        {
+            return t * t * t * (t * (t * 6 - 15) + 10);
+        }
+        private double Lerp(double t, double a, double b)
+        {
+            return a + t * (b - a);
+        }
+        private double Grad(int hash, double x, double y, double z)
+        {
+            int h = hash & 15;
+            double u = h < 8 ? x : y;
+            double v = h < 4 ? y : h == 12 || h == 14 ? x : z;
+            return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
+        }
+        private double Noise(double x, double y, double z)
+        {
+            int X = (int)Math.Floor(x) & 255;
+            int Y = (int)Math.Floor(y) & 255;
+            int Z = (int)Math.Floor(z) & 255;
+
+            x -= Math.Floor(x);
+            y -= Math.Floor(y);
+            z -= Math.Floor(z);
+
+            double u = Fade(x);
+            double v = Fade(y);
+            double w = Fade(z);
+
+            int A = p[X] + Y;
+            int AA = p[A] + Z;
+            int AB = p[A + 1] + Z;
+            int B = p[X + 1] + Y;
+            int BA = p[B] + Z;
+            int BB = p[B + 1] + Z;
+
+            return Lerp(w, Lerp(v, Lerp(u, Grad(p[AA], x, y, z),
+                                Grad(p[BA], x - 1, y, z)),
+                        Lerp(u, Grad(p[AB], x, y - 1, z),
+                                Grad(p[BB], x - 1, y - 1, z))),
+                Lerp(v, Lerp(u, Grad(p[AA + 1], x, y, z - 1),
+                                Grad(p[BA + 1], x - 1, y, z - 1)),
+                        Lerp(u, Grad(p[AB + 1], x, y - 1, z - 1),
+                                Grad(p[BB + 1], x - 1, y - 1, z - 1))));
+        }
+        public double[,] GetNoiseArray(int sizeX, int sizeY, double noiseScale = 0.1)
+        {
+            double[,] outputArray = new double[sizeX, sizeY];
+            for (int y = 0; y < sizeY; y++)
+            {
+                for (int x = 0; x < sizeX; x++)
+                {
+                    outputArray[x, y] = Noise(x * noiseScale, y * noiseScale, 0);
+                }
+            }
+            return outputArray;
+        }
+    }
+}
