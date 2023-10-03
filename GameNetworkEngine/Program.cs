241e2633ee065e9016f17b1caffcd37e29597338
Alexander Babko <106757034+kmnless@users.noreply.github.com>
2023-10-03 04:23:40 +0300

    Delete GameNetworkEngine directory

--- GameNetworkEngine/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿const string host = "127.0.0.1";
-const int port = 8080;
-
-IServerHandler handler = new TextServerHandler()
-{
-    EncodingType = EncodingType.UTF8
-};
-Server server = new Server(host, port, handler);
