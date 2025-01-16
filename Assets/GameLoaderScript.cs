1e071eeee95d71cf5fda1c95fc05426cd6489ea0
Babk_ut38@student.itstep.org <Babk_ut38@student.itstep.org>
2025-01-16 04:53:12 +0200

    sync

--- Assets/GameLoaderScript.cs
+++ Assets/GameLoaderScript.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class GameLoaderScript : MonoBehaviour
 {
@@ -13,6 +14,7 @@ public class GameLoaderScript : MonoBehaviour
     [SerializeField] private GameObject bases;
     [SerializeField] private GameObject outposts;
     [SerializeField] private PlayerHandlerScript playerHandler;
+    [SerializeField] private GameObject playerPrefab;
     [SerializeField] private NavMeshPlus.Components.NavMeshSurface navigator;
 
     private void Awake()
@@ -29,6 +31,24 @@ public class GameLoaderScript : MonoBehaviour
                 GlobalVariableHandler.Instance.FieldSizeY, 
                 GlobalVariableHandler.Instance.BuildingsField, 
                 spriteSize, bases, flags, outposts);
+
+
+            foreach (var p in GlobalVariableHandler.Instance.Players)
+            {
+                Vector3 position = new Vector3(GlobalVariableHandler.Instance.CellSize / 200f, GlobalVariableHandler.Instance.CellSize / 200f, 0);
+
+                // sample
+
+                //var b = bases.GetComponents<GameObject>();
+                //for (int i = 0; i < GlobalVariableHandler.Instance.PlayerCount; i++)
+                //{
+                //    if(b[i].name == p.Name)
+                //        position = b[i].transform.position;
+                //}
+                GameObject playerObject = Instantiate(playerPrefab, position, Quaternion.identity);
+                playerObject.name = $"Player{p.Id}";
+            }
+            
         }
         catch(Exception ex) 
         {
