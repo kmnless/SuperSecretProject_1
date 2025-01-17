c506df31e8bbcdaa4f58e093d22041942ee4c9d5
Babk_ut38@student.itstep.org <Babk_ut38@student.itstep.org>
2025-01-17 06:21:36 +0200

    sync

--- Assets/ServerHandler.cs
+++ Assets/ServerHandler.cs
@@ -117,7 +117,13 @@ public class ServerHandler : MonoBehaviour
         {
             player.Color = GlobalVariableHandler.Instance.Colors[ColorCount++];
             GlobalVariableHandler.Instance.Players.Add(player);
-            clientRpcHandler.SetMyIndexClientRpc(player.Id);
+            clientRpcHandler.SetMyIndexClientRpc(player.Id, new ClientRpcParams
+            {
+                Send = new ClientRpcSendParams
+                {
+                    TargetClientIds = new[] { request.ClientNetworkId }
+                }
+            });
         }
 
         PlayersReadyList.Add(new PlayerReadyStatus(player.Id, nickname));
